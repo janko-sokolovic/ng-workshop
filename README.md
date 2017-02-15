862b0c1bc2e7b59e250154c6bdcaeabef2dda8b5
jasofalcon <jasofalcon@gmail.com>
2017-02-15 16:33:19 +0100

    Set-up dev environment. Gulp, browserify, angularjs, browsersync, gulp tasks

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-"# ng-workshop" 
+# Welcome to ng-workshop
+
+This is a source-code which is used for angular workshop. Follow the steps instructed in the presentation to fulfill the workshop.
+
+## Starting the project.
+
+```javascript
+npm install
+gulp
+```
+
+After starting the project, browser-sync will be initiated alongside with watch task, so you will not have to refresh/rebuild at any time. All file changes in /src folder should be immediately reflected in browser. If you don't have gulp installed globally, you will have to install it globally with npm
+
+```javascript
+npm i -g gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('clean', function() {
+     return del(['dist/']);
+});
+
+gulp.task('build', ['clean'], function(){
+  gulp.src('./src/**/*.html')
+     .pipe(gulp.dest('dist/'));
+
+  return gulp.src(['src/app.js'])
+     .pipe(browserify({
+         insertGlobals: true,
+         debug: true
+     }))
+     .pipe(concat('bundle.js'))
+     .pipe(gulp.dest('dist/js'))
+     .pipe(browserSync.reload({
+         stream: true
+     }));
+});
+
+gulp.task('browser-sync', ['build'], function() {
+    browserSync.init({server: {baseDir: "./dist/"}});
+});
+
+gulp.task('watch', [ 'browser-sync'], function() {
+  gulp.watch(['src/**/*.js', 'src/**/*.html'], ['build']);
+});
+
+gulp.task('default', ['watch']);
